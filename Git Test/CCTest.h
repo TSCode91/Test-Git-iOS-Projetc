f3d7243acf2dd1486f67fbf112cca30beb41f5c4
Tsofatse Emmanuel <emmat.9101@gmail.com>
2014-05-06 10:53:29 +0000

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Tsofatse Emmanuel on 5/6/14.
+//  Copyright (c) 2014 RED. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,14 @@
+
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Tsofatse Emmanuel on 5/6/14.
+//  Copyright (c) 2014 RED. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
